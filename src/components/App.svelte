e0db12ba6e701b5c3c5bfb65bf5ecabfd81d6b48
Charles E. Lehner <charles.lehner@spruceid.com>
2021-02-19 15:22:16 -0500

    Disable Google

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -32,10 +32,12 @@
 </script>
 
 <svelte:head>
+    <!--
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
         rel="stylesheet"
     />
+    -->
 </svelte:head>
 
 <main
