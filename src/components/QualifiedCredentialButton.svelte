4287e81a1303d89d9cea501ddad86aa9878fe0cc
K. Rhoda <kelseydrhoda@gmail.com>
2021-02-22 23:12:17 -0700

    allow creation and caching of unsigned prototype Sybil VCs

--- src/components/QualifiedCredentialButton.svelte
+++ src/components/QualifiedCredentialButton.svelte
@@ -18,13 +18,13 @@
 	{#if statusEntry.status[credentialKey].cached}
 		<button on:click={issueFunc}>Issue {credentialTitle} Credential</button>
 	{:else if !statusEntry.live && statusEntry.status[credentialKey].qualified}
-		<button disabled={true}>Create Trade Activity Credential</button>
+		<button disabled={true}>Create {credentialTitle} Credential</button>
 		<p style="color:white">
 			Cannot create new credential with disconnected wallet
 		</p>
 		<!-- TODO: Add qualifications check here -->
 	{:else if statusEntry.status[credentialKey].qualified}
-		<button on:click={createFunc}>Create Trade Activity Credential</button>
+		<button on:click={createFunc}>Create {credentialTitle} Credential</button>
 	{:else if statusEntry.status[credentialKey].qualified_err}
 		<p style="color:white;">
 			Does not qualify for {credentialTitle} Credential: {statusEntry.status[credentialKey]
