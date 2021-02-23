69015f36e626c4e759186ca172873343da473fd6
Gregório Granado Magalhães <greg.magalhaes@gmail.com>
2021-02-23 13:48:39 -0300

    Controller ui

---
+++ src/components/ControllerLayout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import SecondaryButton from './SecondaryButton.svelte';
+	import IconTitle from './IconTitle.svelte';
+import Menu from './Menu.svelte';
+    export let title;
+    export let icon;
+</script>
+
+<div class="flex flex-col flex-grow z-10">
+    <Menu></Menu>
+    <IconTitle {title} {icon} />
+    <SecondaryButton label="Connect Wallet" />
+</div>
---
+++ src/components/IconTitle.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let title;
+    export let icon;
+</script>
+
+<div class="flex flex-col items-center justify-center">
+    <img src={icon} alt="{title}-icon" class="w-16 h-16" />
+    <h1 class="md:mt-12 md:mb-16 mb-4 font-semibold text-6xl text-white text-center">
+        { title }
+    </h1>
+</div>
