3a347d79f066ff58b5a2e10311bd4846bf20708f
K. Rhoda <kelseydrhoda@gmail.com>
2021-02-23 20:27:30 -0700

    add basic styling to select

--- src/routes/Unicred.svelte
+++ src/routes/Unicred.svelte
@@ -575,6 +575,7 @@
 	{/if}
 	<label for="currentAddress">Choose An Address</label>
 	<select
+		class="text-white p-4 text-left rounded-2xl max-w-sm mx-auto flex items-center h-16 w-full bg-blue-998 border-2 border-blue-997 mb-6"
 		bind:value={currentAddress}
 		on:change={() => {
 			checkQualifications(currentAddress);
