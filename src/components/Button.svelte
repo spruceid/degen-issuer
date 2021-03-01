a278ac47a66e82ec4dd1f3b9eb882d7d83ab221e
K. Rhoda <kelseydrhoda@gmail.com>
2021-03-01 16:03:44 -0700

    Add user supplied did for usage without credible

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,35 +1,39 @@
 <script>
-    export let label;
-    export let icon;
-    export let href;
-    export let disabled = false;
+	export let label;
+	export let icon;
+	export let href;
+	export let onClick = () => {};
+	export let disabled = false;
 </script>
 
-<style>
-.disabled {
-    opacity: 0.5;
-}
-.disabled button {
-    cursor: default;
-}
-.disabled button:focus {
-    outline: none;
-}
-</style>
-
-<a href={disabled ? null : href}
-    class="max-w-sm mx-auto my-2 w-full" class:disabled
+<a
+	href={disabled ? null : href}
+	class="max-w-sm mx-auto my-2 w-full"
+	class:disabled
 >
-    <button
-        class="bg-blue-990 text-white py-4 text-left rounded-2xl font-semibold flex items-center w-full h-18"
-    >
-        {#if icon}
-            <div class="w-24 flex justify-center">
-                <img alt="{label}-logo" src={icon} />
-            </div>
-            {label}
-        {:else}
-            <p class="justify-center flex w-full">{label}</p>
-        {/if}
-    </button>
+	<button
+		class="bg-blue-990 text-white py-4 text-left rounded-2xl font-semibold flex items-center w-full h-18"
+		on:click={onClick}
+	>
+		{#if icon}
+			<div class="w-24 flex justify-center">
+				<img alt="{label}-logo" src={icon} />
+			</div>
+			{label}
+		{:else}
+			<p class="justify-center flex w-full">{label}</p>
+		{/if}
+	</button>
 </a>
+
+<style>
+	.disabled {
+		opacity: 0.5;
+	}
+	.disabled button {
+		cursor: default;
+	}
+	.disabled button:focus {
+		outline: none;
+	}
+</style>
